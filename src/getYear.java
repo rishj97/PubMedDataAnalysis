230909e774f943e5887256e6249b7e01a4a88785
rishabh <rishabh@dyn1182-111.wlan.ic.ac.uk>
2016-05-28 23:52:38 +0100

    Added more functionlity to getYear.

--- src/getYear.java
+++ src/getYear.java
@@ -8,37 +8,67 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class getYear {
+
+
+  //TODO: ensure URL connection is estalished only once.
+
+
+  static String URLConnection;
+  String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+
+
   public static void main(String[] args) throws IOException {
     String charset = java.nio.charset.StandardCharsets.UTF_8.name();
-    URLConnection connection = new URL("http://eutils.ncbi.nlm.nih" +
-        ".gov/entrez/eutils/esearch" +
-        ".fcgi?db=pubmed&term=2011[pdat]&retmax=1000").openConnection();
+    String url = createURL("esearch", "pubmed", "term=2011[pdat]&retmax=10");
+
+
+    URLConnection connection = new URL(url).openConnection();
     connection.setRequestProperty("Accept-Charset", charset);
     InputStream response = connection.getInputStream();
 
-    /*try (Scanner scanner = new Scanner(response)) {
-      String responseBody = scanner.useDelimiter("\\A").next();
-      System.out.println(responseBody);
-    }*/
+//    try (Scanner scanner = new Scanner(response)) {
+//      String responseBody = scanner.useDelimiter("\\A").next();
+//      System.out.println(responseBody);
+//    }
 
+
+    url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink" +
+        ".fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_citedin&id=10557283&tool=my_tool&email=my_email@example.com";
+
+
+    connection = new URL(url).openConnection();
+    connection.setRequestProperty("Accept-Charset", charset);
+    response = connection.getInputStream();
+
+//    try (Scanner scanner = new Scanner(response)) {
+//      String responseBody = scanner.useDelimiter("\\A").next();
+//      System.out.println(responseBody);
+//    }
 
     // create a new DocumentBuilderFactory
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
     try {
-      // use the factory to create a documentbuilder
+      // use the factory to create a documentBuilder
       DocumentBuilder builder = factory.newDocumentBuilder();
 
       // create a new document from input stream
       Document doc = builder.parse(response);
 
       // get the first element
-      Element element = doc.getDocumentElement();
+      Element root = doc.getDocumentElement();
+
+      System.out.println(root);
 
       // get all child nodes
-      NodeList nodes = element.getChildNodes();
+      NodeList nodes = root.getChildNodes();
+      System.out.println(nodes.getLength());
+      System.out.println(nodes.item(0));
+      System.out.println(nodes.item(1));
+      System.out.println(nodes.item(2));
 
       // print the text content of each child
       for (int i = 0; i < nodes.getLength(); i++) {
@@ -47,5 +77,34 @@ public class getYear {
     } catch (Exception ex) {
       ex.printStackTrace();
     }
+  }
+
+
+  static String createURL(String e_type, String data_base, String appender) {
+    String str = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/" + e_type + "" +
+        ".fcgi?db=" + data_base + "&" + appender;
+    return str;
+  }
+
+
+  static Scanner getCitations(String pmid) throws IOException {
+
+    String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+
+
+    String url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink" +
+        ".fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_citedin&" + pmid + "&tool" +
+        "=my_tool&email=my_email@example.com";
+
+
+    URLConnection connection = new URL(url).openConnection();
+    connection.setRequestProperty("Accept-Charset", charset);
+    InputStream response = connection.getInputStream();
+
+    try (Scanner scanner = new Scanner(response)) {
+      return scanner;
+    }
+
+
   }
 }
