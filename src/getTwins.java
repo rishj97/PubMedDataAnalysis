7f42d1086b81b9571df40d5fb40ff1e01db17256
rishabh <rishabh@dyn1182-111.wlan.ic.ac.uk>
2016-05-30 13:33:56 +0100

    Implemented RandomAccessFile reader for the purpose of file pointers when reading the year_data file.

--- src/getTwins.java
+++ src/getTwins.java
@@ -6,10 +6,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -17,29 +14,60 @@ public class getTwins {
 
   static int retstart_init = 0;
   static int retmax_limit = 10000;
-  static String file_name = "year_data.txt";
+  static String file_name = "year_data";
+  static int START_YEAR = 1996;
+  static int END_YEAR = 1996;
+  static boolean isLoaded = true;
 
 
   //TODO: ensure URL connection is estalished only once.
 
   public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
     int i;
-    int START_YEAR = 1996;
-    int END_YEAR = 1996;
-    for (i = END_YEAR; i >= START_YEAR; i--) {
-      loadPapers(i);
+    //Loading data year by year in different files.
+    if (!isLoaded) {
+      for (i = END_YEAR; i >= START_YEAR; i--) {
+        loadPapers(i);
+      }
     }
+
+    //TODO:Going through each file and finding twins.
+
+    for (int year = END_YEAR; year >= START_YEAR; year--) {
+
+      RandomAccessFile file = new RandomAccessFile(file_name + year + ".txt",
+          "r");
+
+      while (true) {
+
+
+        int PMID_i = Integer.parseInt(file.readLine());
+        System.out.println(PMID_i);
+        long file_pointer = file.getFilePointer();
+
+        for (int j=1; j <= 100; j++) {
+        int PMID_j = Integer.parseInt(file.readLine());
+          //TODO: Compare pmid_i and pmid_j
+        }
+
+        file.seek(file_pointer);
+      }
+
+
+//      System.out.println(ob);
+
+
+    }
+
+
   }
 
   private static void loadPapers(int year) throws IOException, ParserConfigurationException, SAXException {
 
     String charset = java.nio.charset.StandardCharsets.UTF_8.name();
 
-    FileWriter file_writer = new FileWriter(file_name);
-    PrintWriter print_writer = new PrintWriter( file_writer, true );
-
-
-
+    FileWriter file_writer = new FileWriter(file_name + year + ".txt");
+    PrintWriter print_writer = new PrintWriter(file_writer, true);
 
 
     int flag = 1;
@@ -55,34 +83,34 @@ public class getTwins {
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
 
-        // use the factory to create a documentBuilder
-        DocumentBuilder builder = factory.newDocumentBuilder();
+      // use the factory to create a documentBuilder
+      DocumentBuilder builder = factory.newDocumentBuilder();
 
-        // create a new document from input stream
-        Document doc = builder.parse(response);
+      // create a new document from input stream
+      Document doc = builder.parse(response);
 
 
-        // get the first element
-        Element root = doc.getDocumentElement();
+      // get the first element
+      Element root = doc.getDocumentElement();
 
-        // get all child nodes
-        NodeList idList_List = root.getElementsByTagName("IdList");
-        Element idList_element = (Element) idList_List.item(0);
-        NodeList ids_nodes = idList_element.getElementsByTagName("Id");
-        if (ids_nodes.getLength() == 0) {
-          flag = 0;
-        }
-        for (int j = 0; j < ids_nodes.getLength(); j++) {
-          Element e = (Element) ids_nodes.item(j);
+      // get all child nodes
+      NodeList idList_List = root.getElementsByTagName("IdList");
+      Element idList_element = (Element) idList_List.item(0);
+      NodeList ids_nodes = idList_element.getElementsByTagName("Id");
+      if (ids_nodes.getLength() == 0) {
+        flag = 0;
+      }
+      for (int j = 0; j < ids_nodes.getLength(); j++) {
+        Element e = (Element) ids_nodes.item(j);
 
-          if(e.getTextContent().equals("8942774")) {
-            System.out.println("its there");
-            flag=0;
+//          if(e.getTextContent().equals("8942774")) {
+//            System.out.println("its there");
+//            flag=0;
+//
+//          }
 
-          }
-
-          print_writer.printf( "%s" + "%n" , e.getTextContent());
-        }
+        print_writer.printf("%s" + "%n", e.getTextContent());
+      }
 
     }
     print_writer.close();
@@ -92,7 +120,7 @@ public class getTwins {
 
     String str = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch" +
         ".fcgi?db=pubmed&term=" + year +
-        "[pdat]&retmax="+retmax_limit+"&retstart=" +
+        "[pdat]&retmax=" + retmax_limit + "&retstart=" +
         ret_start;
     return str;
   }
